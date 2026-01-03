3b4cdc44654a6d0c82f34ae13814ec4690c73f96
SubhanHakim <subhanhakim07@gmail.com>
2026-01-03 15:43:56 +0700

    update hero section

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -28,7 +28,7 @@ export default function HeroSection() {
                     </div>
 
                     <div className="relative">
-                        <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-black tracking-tighter leading-tight text-gray-900 mb-4 sm:mb-6 text-center lg:text-left">
+                        <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-black tracking-tighter leading-[0.95] text-gray-900 mb-4 sm:mb-6 text-center lg:text-left">
                             ZYKO, <br />
                             Leader of the <br />
                             <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-green-400">Radpals</span>
